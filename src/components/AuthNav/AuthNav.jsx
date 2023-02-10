976d8b4473b925fdc91dc1c89eaa6fb01f285a7b
VerbitskaAlisa <v.alica@ukr.net>
2023-02-10 17:28:13 +0200

    done

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -7,11 +7,11 @@ export default function AuthNav () {
     
     return (
         <Box display="flex" alignItems="center">
-        <Link as={NavLink} to="login" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Sign In</Link>
+        <Link as={NavLink} to="/login" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Sign In</Link>
         <Center height='50px' ml="2" mr="2">
           <Divider orientation='vertical' />
          </Center>
-        <Link as={NavLink} to="register" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Register</Link>
+        <Link as={NavLink} to="/register" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Register</Link>
    </Box>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,8 +6,8 @@ const setActive = ({isActive}) => ({borderBottom: isActive ? "2px solid #B83280"
 export default function Navigation () {
     return (
         <Box as="nav">
-                    <Link as={NavLink} to="home" mr="4" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Home</Link>
-                    <Link as={NavLink} to="contacts" mr="4" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Contacts</Link>
+                    <Link as={NavLink} to="/home" mr="4" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Home</Link>
+                    <Link as={NavLink} to="/contacts" mr="4" fontSize="3xl" style={setActive} _hover={{borderBottom: "none"}}>Contacts</Link>
                 </Box>
     )
 }
